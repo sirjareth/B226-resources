99c89b87895a63807370eb19a1c476721b6f8ec8
sirjareth <jarethronquillo12@gmail.com>
2026-06-20 15:30:09 +0800

    Add discussion code for day2 and day3

---
+++ src/day3/discussion/SampleCode.java
@@ -0,0 +1,19 @@
+
+package day3.discussion; // package
+
+public class SampleCode { // class
+    
+    public static void main(String[] args) { // main method
+        System.out.println("Hello World"); // print statement
+    } 
+}   
+
+// Syntax -> structure of code
+// Syntax Rule -> Syntax Error
+// Main Method (psvm + tab) -> starting point of programs execution
+// Print Statement (sout + tab) -> it tells the computer to print something
+/*
+    2 Types of Comments
+    1. Single Line Comment
+    2. Multi Line Comment
+*/
