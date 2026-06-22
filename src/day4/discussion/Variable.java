40acb36fcd4bf851e50c7244596f1bd56e04c7b5
Jareth <jarethronquillo12@gmail.com>
2026-06-22 17:48:35 +0800

    Add code discussion for day4

---
+++ src/day4/discussion/Variable.java
@@ -0,0 +1,48 @@
+
+package day4.discussion;
+
+
+public class Variable {
+    
+    public static void main(String[] args) {
+        // Mini-Activity
+        // Requirements: Create a variable for firstName,  
+        // middleInitial, lastName and assign values to it.
+        // Expected Output: Jareth B. Ronquillo
+        
+        String firstName = "Jareth";
+        String middleInitial = "B.";
+        String lastName = "Ronquillo";
+        
+        System.out.println("Full Name: " + firstName + " " +  middleInitial + 
+                " " + lastName);
+        
+        
+        // initialization
+      // DT     var     value
+        String timba = "tubig";
+        
+        // begas
+        timba = "bigas";
+        
+        // Printing a variable
+        //System.out.println(timba);
+        
+        // 26, 20, 18, 22, 24
+        int age = 26, age2 = 20, age3 = 18, age4 = 22;
+        
+        //System.out.println("Age: " + age4);
+        // Age: 22
+        // concatenation(+) -> join values
+        // 38.5
+        // declaration
+        double temp;
+        
+        
+        // assignment
+        temp = 38.5;
+        //System.out.println("Temp: " + temp);
+        
+        
+    }
+}
