27d72c6c79ec4339cfb0f365af7a31fc48543d9a
Jareth <jarethronquillo12@gmail.com>
2026-06-23 16:01:24 +0800

    Add discussion code for Day 5

---
+++ src/day5/discussion/ArithmeticOperator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day5.discussion;
+
+
+public class ArithmeticOperator {
+    // + - * / %
+    public static void main(String[] args) {
+        int num = 55;
+        int num2 = 10;
+        int sum = num + num2;
+        
+        System.out.println("Sum: " + sum);
+        System.out.println(num - num2);
+        System.out.println(num * num2);
+        System.out.println(num / num2);
+        System.out.println(num % num2);
+    }
+}
---
+++ src/day5/discussion/AssignmentOperator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day5.discussion;
+
+
+public class AssignmentOperator {
+    // 
+    public static void main(String[] args) {
+        int num = 10; // Simple Assignment
+        
+        // Compound Assignment
+        num += 10;
+        num -= 5;
+        num *= 5;
+        num /= 5;
+        num %= 2;
+        System.out.println(num);
+        
+        
+    }
+}
