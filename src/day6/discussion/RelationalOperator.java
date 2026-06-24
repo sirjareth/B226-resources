0ca274b22421de94ce7e5b5552f71eadfe0667a9
Jareth <jarethronquillo12@gmail.com>
2026-06-24 16:18:27 +0800

    Add discussion code for Day 6

---
+++ src/day6/discussion/RelationalOperator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day6.discussion;
+
+
+public class RelationalOperator {
+    
+    public static void main(String[] args) {
+        // < > <= >= == !=
+        
+        int num = 100;
+        int num2 = 200;
+        
+        System.out.println(num < num2);
+        System.out.println(num  > num2);
+        System.out.println(num <= num2);
+        System.out.println(num >= num2);
+        System.out.println(num == num2);
+        System.out.println(num != num2);
+        
+    }
+}
---
+++ src/day6/discussion/TernaryOperator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day6.discussion;
+
+
+public class TernaryOperator {
+    
+    public static void main(String[] args) {
+        // syntax
+        // (cond) ? true block : false block;
+        // Positive or Negative
+        int num = -1;
+        
+        String result = (num > 0) ? "Positive" : "Negative";
+        //System.out.println(result);
+        
+        // Fever or Normal Temp
+        
+        float temp = 38.5f;
+        
+        String tempResult = temp >= 38.5 ? "Fever" : "Normal Temp";
+        System.out.println(tempResult);
+    }
+}
