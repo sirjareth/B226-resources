2b09049e29df032b9624dd824f336615d24aa50b
Jareth <jarethronquillo12@gmail.com>
2026-06-26 17:10:10 +0800

    Add code discussion for day 8

---
+++ src/day8/discussion/AgeClassificationNested.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day8.discussion;
+
+import java.util.Scanner;
+
+public class AgeClassificationNested {
+
+    public static void main(String[] args) {
+        /* Program Requirement: Age Classification with Nested If Statement
+        
+        -> Input: Age (integer) from the user using the Scanner
+        
+        -> Processing / Logic: 
+            Age: 0 - 12 -> Child 
+                Age: 0 - 5 ->  Specifically, Toddler
+                Age: 6 - 12 ->  Specifically, Pre-Teen
+            Age: 13 - 19 -> Teenager
+                Age: 13 - 15 ->  Specifically, Eary Teen
+                Age: 16 - 19 ->  Specifically, Late Teen
+            Age: 20 - 59 -> Adult
+                Age: 20 - 35 ->  Specifically, Young Adult
+                Age: 36 - 59 ->  Specifically, Middle-Aged
+            Age: 60 - 120 -> Senior Citizen
+                Age: 60 - 75 ->  Specifically, Young Senior
+                Age: 76 - 120 ->  Specifically, Elderly
+            Invalid Age -> Invalid Input
+         */
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = input.nextInt();
+        // Validation
+
+        if (age >= 0 && age <= 120) {
+            
+        } else {
+            System.out.println("Invalid Age Entered.");
+        }
+        
+        
+        
+        
+        if (age <= 12) { // 0 - 12  - outer if 
+                System.out.println("You are a child");
+                if (age <= 5) { // toddler - inner if
+                    System.out.println("Specifically, you are Toddler.");
+                } else {
+                    System.out.println("Specifically, you are Pre-Teen.");
+                }
+            } else if (age <= 19) { // 13 - 19
+                System.out.println("You are teenager");
+            } else if (age <= 59) { // 20 - 59
+                System.out.println("You are adult");
+            } else if (age <= 120) { // 60 - 120
+                System.out.println("You are senior citizen");
+            } 
+
+        // alt + shift + f
+        // code minimization
+    }
+}
---
+++ src/day8/discussion/LoginProgram.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day8.discussion;
+
+import java.util.Scanner;
+
+
+public class LoginProgram {
+    // User Login Program
+    public static void main(String[] args) {
+        // Create Scanner object to accept input from the user
+        Scanner input = new Scanner(System.in);
+        
+        // Predefined username and password (hardcoded for demo purposes)
+        String username = "admin";
+        String password = "admin123";
+        
+        System.out.println("* Login *");   
+        System.out.print("Enter username: "); // Ask user for username
+        String loginUsername =  input.nextLine(); // Read user input and stores it to loginUsername variable
+        System.out.print("Enter password: "); // Ask user for password
+        String loginPassword = input.nextLine(); // Read user input and stores it to loginPassword variable
+        
+        // Check if both username and password match the predefined values
+        if (loginUsername.equals(username) && loginPassword.equals(password)) {
+            System.out.println("\nLogin successful! " + "Welcome, " + username); 
+        } else {
+            System.out.println("\nLogin Failed. Invalid username or password.");
+        }
+        
+        // close the scanner
+        input.close();
+    }
+}
